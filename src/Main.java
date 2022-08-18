3e5f04d127ad0bd37d63b1c0b4695770a8ad5292
Bavykin Grisha <bavykin.grisha@syk.fi>
2022-08-18 17:32:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 2;
+        if (a==b) {
+            System.out.println(a*a+b*b);
+        }
+        else {
+            System.out.println(a+b);
+        }
+    }
+}
